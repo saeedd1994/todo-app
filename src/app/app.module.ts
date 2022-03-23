fc29293c9c024324e997abecb9f8b1c91eb89aab
saeed <saeed.da1994@gmail.com>
2022-03-23 17:03:47 +0430

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddToDoComponent } from './components/add-to-do/add-to-do.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddToDoComponent,
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-to-do/add-to-do.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-to-do',
+  templateUrl: './add-to-do.component.html',
+  styleUrls: ['./add-to-do.component.scss']
+})
+export class AddToDoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
